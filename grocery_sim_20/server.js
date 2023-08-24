1437f8dcf41bc3e34bd6118806d639864e229e3b
Bernhard Mayrhofer <35584158+BernhardMayrhofer@users.noreply.github.com>
2023-08-24 11:08:39 +0200

    Update server.js

--- grocery_sim_20/server.js
+++ grocery_sim_20/server.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(80);
 
-console.log(__dirname,'Now the server is running in url: http://127.0.0.1:8080');
+console.log(__dirname,'Now the server is running inside docker in url: http://127.0.0.1:80');
+console.log(__dirname,'You can access the docker server on your PC on: http://127.0.0.1:8080');
